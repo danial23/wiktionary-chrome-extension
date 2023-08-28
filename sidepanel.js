69c4d16799904ebcf451a157a38847091f7c1469
danial23 <admin@danial23.com>
2023-08-28 00:10:35 -0400

    Simplify spaghetti code

--- sidepanel.js
+++ sidepanel.js
@@ -1,26 +1,19 @@
-var currentWord = ""; //as shown on extension panel
 var isOnCooldown = false; //will delay sending new requests during cooldown
 const requestDelay = 1000; //delay amount in ms
 const cooldownDuration = 3000;
 var timeoutId = -1;
 
-chrome.runtime.onMessage.addListener(function(selection) {
+chrome.runtime.onMessage.addListener(function (selection) {
   const word = selection.trim();
-
-  if (currentWord === word) {
-    clearTimeout(timeoutId);
-    getCooldown();
-    return;
-  }
-
-  if (word === "") {
-    return;
-  }
 
   if (isOnCooldown) {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(getWordDefinition, requestDelay, word);
   } else {
+    isOnCooldown = true;
+    timeoutId = setTimeout(() => {
+      isOnCooldown = false;
+    }, cooldownDuration);
     getWordDefinition(word);
   }
 });
@@ -28,16 +21,7 @@ chrome.runtime.onMessage.addListener(function(selection) {
 document.getElementById("wiktionary").focus();
 
 function getWordDefinition(word) {
-  getCooldown();
-
   document.getElementById("wiktionary").src =
     "https://en.m.wiktionary.org/wiki/" + word;
   currentWord = word;
-}
-
-function getCooldown() {
-  isOnCooldown = true;
-  timeoutId = setTimeout(() => {
-    isOnCooldown = false;
-  }, cooldownDuration);
 }
